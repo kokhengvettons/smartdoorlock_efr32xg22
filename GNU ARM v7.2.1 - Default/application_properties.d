9585b5b7d5776613a945586dd94ed9309693862a
kokhengvettons <kokheng.l@vettons.com>
2020-08-11 14:37:49 +0800

    Remove build files

--- GNU ARM v7.2.1 - Default/application_properties.d
+++
@@ -1,4 +0,0 @@
-application_properties.o: ../application_properties.c \
- C:\Users\kokhe\SimplicityStudio\v4_workspace\smartdoorlock_efr32xg22\platform\bootloader\api/application_properties.h
-
-C:\Users\kokhe\SimplicityStudio\v4_workspace\smartdoorlock_efr32xg22\platform\bootloader\api/application_properties.h:
--- GNU ARM v7.2.1 - Default/platform/emlib/src/em_assert.d
+++
@@ -1,4 +0,0 @@
-platform/emlib/src/em_assert.o: ../platform/emlib/src/em_assert.c \
- C:\Users\kokhe\SimplicityStudio\v4_workspace\smartdoorlock_efr32xg22\platform\emlib\inc/em_assert.h
-
-C:\Users\kokhe\SimplicityStudio\v4_workspace\smartdoorlock_efr32xg22\platform\emlib\inc/em_assert.h:
