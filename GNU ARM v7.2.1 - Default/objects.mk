3b72e9b96fb97c2640dec984da0cab01bcdf6318
kokhengvettons <kokheng.l@vettons.com>
2020-09-18 15:58:31 +0800

    change folder directory cause unpdate in GNU ARM v7.2.1 - Default

--- GNU ARM v7.2.1 - Default/objects.mk
+++ GNU ARM v7.2.1 - Default/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := C:/Users/kokhe/SimplicityStudio/v4_workspace/smartdoorlock_efr32xg22/protocol/bluetooth/lib/EFR32MG22/GCC/libbluetooth.a C:/Users/kokhe/SimplicityStudio/v4_workspace/smartdoorlock_efr32xg22/protocol/bluetooth/lib/EFR32MG22/GCC/libmbedtls.a C:/Users/kokhe/SimplicityStudio/v4_workspace/smartdoorlock_efr32xg22/protocol/bluetooth/lib/EFR32MG22/GCC/binapploader.o C:/Users/kokhe/SimplicityStudio/v4_workspace/smartdoorlock_efr32xg22/platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a C:/Users/kokhe/SimplicityStudio/v4_workspace/smartdoorlock_efr32xg22/platform/radio/rail_lib/autogen/librail_release/librail_efr32xg22_gcc_release.a
+USER_OBJS := C:/Users/kokhe/OneDrive/Documents/GitHub/smartdoorlock_efr32xg22/protocol/bluetooth/lib/EFR32MG22/GCC/libbluetooth.a C:/Users/kokhe/OneDrive/Documents/GitHub/smartdoorlock_efr32xg22/protocol/bluetooth/lib/EFR32MG22/GCC/libmbedtls.a C:/Users/kokhe/OneDrive/Documents/GitHub/smartdoorlock_efr32xg22/protocol/bluetooth/lib/EFR32MG22/GCC/binapploader.o C:/Users/kokhe/OneDrive/Documents/GitHub/smartdoorlock_efr32xg22/platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a C:/Users/kokhe/OneDrive/Documents/GitHub/smartdoorlock_efr32xg22/platform/radio/rail_lib/autogen/librail_release/librail_efr32xg22_gcc_release.a
 
 LIBS := -lm
 
